92000ab34ea6f451b8ab3dd389c767f348f65ef9
lavish-sudo <ljhamb91@gmail.com>
2020-10-03 12:58:41 -0700

    file creation script has been modified

--- filecreate.sh
+++ filecreate.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+##creating a dir test and then moving inside it to create and delete files
+mkdir test
+cd test
+
+##creation of five text files
 touch file{1..5}.txt
+
+##deletion of all the files
+rm -rf *
 
